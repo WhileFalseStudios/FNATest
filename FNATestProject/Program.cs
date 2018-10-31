eec44db6db341c3403beed72514565643ffd17bc
Andrew Castillo <valax321@gmail.com>
2018-11-01 09:43:28 +1100

    Empty project commit

---
+++ FNATestProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNATestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
