df4811a20d97b053e540c91b68d4136e51691b1d
Andrew Castillo <valax321@gmail.com>
2018-11-05 09:09:12 +1100

    Draws a crappy texture to demo the new Asset system

--- FNATestProject/FNATestGame.cs
+++ FNATestProject/FNATestGame.cs
@@ -11,18 +11,37 @@ namespace FNATestProject
     public class FNATestGame : Game
     {
         GraphicsDeviceManager graphics;
+        SpriteBatch batch;
+        Texture2D testTex;
 
         public FNATestGame() : base()
         {
-            graphics = new GraphicsDeviceManager(this);            
+            graphics = new GraphicsDeviceManager(this);
+            batch = new SpriteBatch(GraphicsDevice);
         }
 
         protected override void Initialize()
         {
             base.Initialize();
+            Content.LogSearchPaths();
             int dpCount = Content.MountDataPath("test");
             FNALoggerEXT.LogInfo.Invoke($"Found {dpCount} mount points with name test");
-            FNALoggerEXT.LogInfo.Invoke("FNA Test Project: initialised");
+
+            string msg = Content.Load<string>("hellomessage.txt");
+            FNALoggerEXT.LogInfo.Invoke($"A very important announcement: {msg}");
+            Content.Release("hellomessage.txt");
+
+            testTex = Content.Load<Texture2D>("textures/texture2dtest.png");
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {            
+            GraphicsDevice.Clear(Color.Black);
+            batch.Begin();
+            batch.Draw(testTex, new Vector2(100, 100), Color.White);
+            batch.End();
+
+            base.Draw(gameTime);
         }
     }
 }
